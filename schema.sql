8bbefc36fa3e21650018e22525e19d147ceb9481
Alex McDonald <acm4219@gmail.com>
2020-10-21 18:54:27 -0500

    adding passport items and pushing second table up to main

--- schema.sql
+++ schema.sql
@@ -14,4 +14,15 @@ primary key
     (id)
 );
 
---Jokester table, use this to get the total number of jokes in a ranking--
+    --Jokester table, use this to get the total number of jokes in a ranking--
+    create table jokesters
+    (
+        id int
+        auto_increment not NULL,
+ user varchar
+        (250),
+ password varchar
+        (250),
+ primary key
+        (id)
+);
