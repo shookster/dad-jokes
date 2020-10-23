4e3318f2f973500318a503696113a83752a6d622
Alex McDonald <acm4219@gmail.com>
2020-10-22 19:41:13 -0500

    getting the api Routes all set up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,36 @@
 const express = require("express")
-var router = express.Router();              
+var router = express.Router();   
+const db = require("../models")           
 
-router.get('/', function(req, res) {
-    res.json({ message: 'test, test, this is a test' });   
-});
+router.get("/api/posts/", function(req, res) {
+    db.Jokes.findAll({})
+      .then(function(dbJokes) {
+        res.json(dbJokes);
+      });
+  });
 
+  router.post("/api/submitjoke", function(req, res) {
+    console.log(req.body);
+    db.Jokes.create({
+     name: req.body.name,
+     punchline: req.body.punchline,
+     genre: req.body.genre,
+     rating: req.body.rating
+    })
+      .then(function(dbJokes) {
+        res.json(dbJokes);
+      });
+  });
+  router.post("/api/signup", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
 module.exports= router;
 
 
